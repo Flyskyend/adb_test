ade32ba7e57fcf3d6dd786902e1456b51067dca8
Flyskyend <1286446631@qq.com>
2023-01-04 01:29:33 +0800

    Combine function

--- matchTemplate.py
+++ matchTemplate.py
@@ -1,18 +1,34 @@
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
+import glob
 
-img_rgb = cv.imread('Desktop_1.jpg')
-img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
-template = cv.imread('Desktop_folder.jpg',0)
-w, h = template.shape[::-1]
-res = cv.matchTemplate(img_gray,template,cv.TM_CCOEFF_NORMED)
-threshold = 0.8
-loc = np.where( res >= threshold)
-for pt in zip(*loc[::-1]):
-    print((pt[0] + w, pt[1] + h))
-    cv.rectangle(img_rgb, pt, (pt[0] + w + 40, pt[1] + h + 40), (0,0,255), 2)
+path = 'pubg/pubg_1.jpg'
+# path = 'trxf/trxf_12.jpg'
+for filename in glob.glob(path):
+    print('read ', filename)
+    img_rgb = cv.imread(filename)
+    img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
+    # template = cv.imread('trxf/icon_circle.jpg', 0)
+    template = cv.imread('pubg/pubg_point_yellow.png', 0)
+    w, h = template.shape[::-1]
+    res = cv.matchTemplate(img_gray,template,cv.TM_CCOEFF_NORMED)
+    threshold = 0.4
+    loc = np.where( res >= threshold)
+    for pt in zip(*loc[::-1]):
+        print((pt[0] + w, pt[1] + h))
+        cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
 
-cv.rectangle(img_rgb, (1280, 720), (1300, 768), (0,0,255), 2)
-cv.imwrite('res.png',img_rgb)
+    # template = cv.imread('trxf/icon_circle.jpg', 0)
+    # w, h = template.shape[::-1]
+    # res = cv.matchTemplate(img_gray,template,cv.TM_CCOEFF_NORMED)
+    # threshold = 0.8
+    # loc = np.where( res >= threshold)
+    # for pt in zip(*loc[::-1]):
+    #     print((pt[0] + w, pt[1] + h))
+    #     cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+
+    # cv.rectangle(img_rgb, (1280, 720), (1300, 768), (0,0,255), 2)
+    # cv.imwrite(filename.split('\\')[1].split('.')[0] + "_res.png",img_rgb)
+    cv.imwrite("res.png",img_rgb)
 
--- ocr.py
+++ ocr.py
@@ -1,9 +1,9 @@
 import easyocr
 import glob
 
-path = './*.jpg'
+path = 'trxf/trxf_12.jpg'
 for filename in glob.glob(path):
-    reader = easyocr.Reader(['ch_sim','en']) # this needs to run only once to load the model into memory
+    reader = easyocr.Reader(['ch_sim','en'], gpu=True) # this needs to run only once to load the model into memory
     result = reader.readtext(filename)
     print(result)
 
@@ -12,10 +12,9 @@ for filename in glob.glob(path):
     
     print("===========================")
 
-from PIL import Image,ImageDraw
-image_path='XXXXXXXXXX.jpg'
-image=Image.open(image_path)
-draw = ImageDraw.Draw(image)
-
-draw.polygon([(902,1217),(1288,1215),(1288,1269),(903,1271)], outline=(255,0,0))
-image.show()
+# from PIL import Image,ImageDraw
+# image_path='XXXXXXXXXX.jpg'
+# image=Image.open(image_path)
+# draw = ImageDraw.Draw(image)
+# draw.polygon([(902,1217),(1288,1215),(1288,1269),(903,1271)], outline=(255,0,0))
+# image.show()
