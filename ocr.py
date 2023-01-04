33e358960124ce0e766e37330aa06f722356c6c6
Flyskyend <1286446631@qq.com>
2023-01-04 17:59:24 +0800

    20230104

--- ocr.py
+++ ocr.py
@@ -1,20 +1,20 @@
 import easyocr
 import glob
+from PIL import Image,ImageDraw
 
-path = 'trxf/trxf_12.jpg'
+path = 'trxf/trxf_*.jpg'
 for filename in glob.glob(path):
     reader = easyocr.Reader(['ch_sim','en'], gpu=True) # this needs to run only once to load the model into memory
     result = reader.readtext(filename)
     print(result)
 
+    image=Image.open(filename)
+    draw = ImageDraw.Draw(image)
     for i in range(0, len(result)):
+        draw.polygon([tuple(result[i][0][0]), tuple(result[i][0][1]), tuple(result[i][0][2]), tuple(result[i][0][3])], outline=(255,0,0))
         print(i, result[i])
     
+    # image.show()
+    image.save(filename.split('/')[1].split('.')[0] + "_res.jpg")
+    
     print("===========================")
-
-# from PIL import Image,ImageDraw
-# image_path='XXXXXXXXXX.jpg'
-# image=Image.open(image_path)
-# draw = ImageDraw.Draw(image)
-# draw.polygon([(902,1217),(1288,1215),(1288,1269),(903,1271)], outline=(255,0,0))
-# image.show()
